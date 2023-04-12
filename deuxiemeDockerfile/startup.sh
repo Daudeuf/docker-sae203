7c910b325080c523b97cb5e659e233d6425dd879
Clem76 <39047200+Daudeuf@users.noreply.github.com>
2023-04-12 14:13:53 +0200

    Update startup.sh

--- deuxiemeDockerfile/startup.sh
+++ deuxiemeDockerfile/startup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exec /etc/init.d/mariadb start
+# Start the first process
+/etc/init.d/mariadb start &
+  
+# Start the second process
+/etc/init.d/apache2 start &
+  
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
