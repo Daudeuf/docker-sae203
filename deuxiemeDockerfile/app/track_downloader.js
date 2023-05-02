11ddef06633aa2fcab5f38db654a51592fd98485
Clem76 <39047200+Daudeuf@users.noreply.github.com>
2023-05-02 11:20:48 +0200

    Update track_downloader.js

--- deuxiemeDockerfile/app/track_downloader.js
+++ deuxiemeDockerfile/app/track_downloader.js
@@ -31,7 +31,7 @@ async function downloadSong(link) {
 	// console.log('Attente de l\'élément...');
 	// await page.waitForSelector('#__next > div > div.mt-5.m-auto.text-center > div:nth-child(5) > div > div > div.flex.items-center.justify-end > button');
 	// await page.waitForXPath('//*[@id="__next"]/div/div[2]/div[1]/div/div[2]/button');
-	await page.waitForSelector('button[text="Télécharger"]')
+	await page.waitForSelector('#__next > div > div.mt-5.m-auto.text-center')
 
 	// console.log('Clic sur le deuxième bouton...');
 	await page.click('#__next > div > div.mt-5.m-auto.text-center > div.mb-12.grid.grid-cols-1.gap-3.m-auto > div > div.flex.items-center.justify-end > button');
