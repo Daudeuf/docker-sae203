eb4aeb6e298037bc4d69d4663e60a46dd05cc409
Clem76 <39047200+Daudeuf@users.noreply.github.com>
2023-05-02 10:14:23 +0200

    Update track_finder.js

--- deuxiemeDockerfile/app/track_finder.js
+++ deuxiemeDockerfile/app/track_finder.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 async function findSpotifyTitle(searchTerm) {
 	// console.log('Lancement du navigateur...');
 	const browser = await puppeteer.launch({
-  		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  		args: ['--no-sandbox'],
 		headless: false
 	});
 	const page = await browser.newPage();
